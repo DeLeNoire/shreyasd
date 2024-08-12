0b202bdee398517e6a76d2b1438921603b2754ac
shreyas <134807467+DeLeNoire@users.noreply.github.com>
2024-08-13 02:45:54 +0530

    herolayout

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import App from './Band';
+
+  
+export default function Hero() {
+  return (
+    <div className='flex items-center justify-center  h-screen bg-gradient-to-b from-black to-white'>
+    <div 
+        style={{
+            flex: 1,
+            textAlign: 'center',
+            color: 'black',
+            height: '100vh',
+        }}
+    >
+        <App />
+    </div>
+    </div>
+  )
+}
