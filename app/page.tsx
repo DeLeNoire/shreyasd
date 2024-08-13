9d040aeee137055ac734d0d2331cfed1e450d950
shreyas <134807467+DeLeNoire@users.noreply.github.com>
2024-08-13 22:05:15 +0530

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-24 lg:p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 lg:bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+        <div className="fixed left-0 top-0 flex w-screen lg:w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
         Don't be shy, say hi!&nbsp;
                   {/* Show text link on large screens and above */}
                   <code className="font-mono font-bold hidden lg:block">
